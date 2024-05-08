2a083c0ceb86c845afa7101f936475485aad9280
piresz0 <166669801+piresz0@users.noreply.github.com>
2024-05-08 20:32:06 -0300

    Add files via upload

---
+++ scripts-basico.js
@@ -0,0 +1,24 @@
+//O evento DOM é acionado quando todo o HTML foi completamente carregado
+
+document.addEventListener('DOMContentLoaded', function () {
+
+alert(123);
+
+//TIPOS DE DADOS
+//String: Sequencia de caracter
+//Number: Numeros inteiros ou pontos flutuantes (10.4)
+//Boolean: Retorna dois valores, true ou false
+//object: estrutura de dados que pode armazenar multiplos valores
+//NULL: quando esta vazio
+//array:objeto especial ordenado 
+//function: bloco de codigo reutilizavel
+
+//const É usado para declarar uma variavel cujo valor é permanente
+//const teste =50;
+//console.log(teste);
+
+
+//É usado para declarar uma variaveis que os vsalores podem ser retrebuidos
+//let C = 0;
+
+)}
