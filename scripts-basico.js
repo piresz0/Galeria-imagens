f2d9410510af2ebb049f34f8ea86806d915de13b
piresz0 <166669801+piresz0@users.noreply.github.com>
2024-05-09 21:41:37 -0300

    Update scripts-basico.js

--- scripts-basico.js
+++ scripts-basico.js
@@ -1,37 +1,33 @@
 //O evento DOM é acionado quando todo o HTML foi completamente carregado
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
+  alert(123);
 
-alert(123);
+  //TIPOS DE DADOS
+  //String: Sequencia de caracter
+  //Number: Numeros inteiros ou pontos flutuantes (10.4)
+  //Boolean: Retorna dois valores, true ou false
+  //object: estrutura de dados que pode armazenar multiplos valores
+  //NULL: quando esta vazio
+  //array:objeto especial ordenado
+  //function: bloco de codigo reutilizavel
 
-//TIPOS DE DADOS
-//String: Sequencia de caracter
-//Number: Numeros inteiros ou pontos flutuantes (10.4)
-//Boolean: Retorna dois valores, true ou false
-//object: estrutura de dados que pode armazenar multiplos valores
-//NULL: quando esta vazio
-//array:objeto especial ordenado 
-//function: bloco de codigo reutilizavel
+  //CONST É usado para declarar uma variavel cujo valor é permanente
+  const teste = 50;
+  console.log(teste);
 
-//CONST É usado para declarar uma variavel cujo valor é permanente
-const teste =50
-console.log(teste)
+  //LET É usado para declarar uma variaveis que os vsalores podem ser retrebuidos
+  let C = 0;
+  C = 10;
+  console.log(C);
 
+  const notaMedia = 6;
 
-//LET É usado para declarar uma variaveis que os vsalores podem ser retrebuidos
-let C = 0
-C = 10;
-console.log(C)
-
-const notaMedia = 6;
-
-if(notaMedia >= 6) {
+  if (notaMedia >= 6) {
     alert("aluno passou");
-}
-else if (notaMedia >= 4 && notaMedia < 6) {
-    alert("aluno quase passou")
-}
-else{("aluno reprovou!");
-}
-
-})
+  } else if (notaMedia >= 4 && notaMedia < 6) {
+    alert("aluno quase passou");
+  } else {
+    ("aluno reprovou!");
+  }
+});
